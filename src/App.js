51233f39d35c6565676dd450157afe293e9e0f9d
nicesuits <raion314@outlook.com>
2019-04-03 17:54:46 -0700

    refactor IssueLIst component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,6 @@
 import React from 'react';
+import IssueList from './components/IssueList';
 
-import IssueFilter from './components/IssueFilter';
-import IssueAdd from './components/IssueAdd';
-import IssueTable from './components/IssueTable';
+const App = () => <IssueList />;
 
-const data = [
-  {
-    id: 1,
-    status: 'Open',
-    owner: 'Ravan',
-    created: new Date('2016-08-15'),
-    effort: 5,
-    completionDate: undefined,
-    title: 'Error in console when clicking Add'
-  },
-  {
-    id: 2,
-    status: 'Assigned',
-    owner: 'Eddie',
-    created: new Date('2016-08-16'),
-    effort: 14,
-    completionDate: new Date('2016-08-30'),
-    title: 'Missing bottom border on panel'
-  }
-];
-
-export default () => (
-  <div>
-    <h1>Inventory Table</h1>
-    <IssueFilter />
-    <hr />
-    <IssueTable issues={data} />
-    <hr />
-    <IssueAdd />
-  </div>
-);
+export default App;
--- src/components/IssueList.js
+++ src/components/IssueList.js
@@ -1,5 +1,39 @@
 import React from 'react';
 
-const IssueList = () => <div>This is a placeholder for the issue list.</div>;
+import IssueFilter from './IssueFilter';
+import IssueAdd from './IssueAdd';
+import IssueTable from './IssueTable';
+
+const data = [
+  {
+    id: 1,
+    status: 'Open',
+    owner: 'Ravan',
+    created: new Date('2016-08-15'),
+    effort: 5,
+    completionDate: undefined,
+    title: 'Error in console when clicking Add'
+  },
+  {
+    id: 2,
+    status: 'Assigned',
+    owner: 'Eddie',
+    created: new Date('2016-08-16'),
+    effort: 14,
+    completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel'
+  }
+];
+
+const IssueList = () => (
+  <div>
+    <h1>Inventory Table</h1>
+    <IssueFilter />
+    <hr />
+    <IssueTable issues={data} />
+    <hr />
+    <IssueAdd />
+  </div>
+);
 
 export default IssueList;
