5fd4f0eff2294713a5e085c45dd404fe1f58ed79
nicesuits <raion314@outlook.com>
2019-04-03 18:38:57 -0700

    add state and IssueAdd component

--- src/components/IssueList.js
+++ src/components/IssueList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import IssueFilter from './IssueFilter';
 import IssueAdd from './IssueAdd';
@@ -25,15 +25,40 @@ const data = [
   }
 ];
 
-const IssueList = () => (
-  <div>
-    <h1>Inventory Table</h1>
-    <IssueFilter />
-    <hr />
-    <IssueTable issues={data} />
-    <hr />
-    <IssueAdd />
-  </div>
-);
+class IssueList extends Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  createIssue(newIssue) {
+    const newIssues = this.state.issues.slice();
+    newIssue.id = this.state.issues.length + 1;
+    newIssues.push(newIssue);
+    this.setState({ issues: newIssues });
+  }
+
+  loadData() {
+    this.setState({ issues: data });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Inventory Table</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={this.state.issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue} />
+      </div>
+    );
+  }
+}
 
 export default IssueList;
