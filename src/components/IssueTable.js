13ee198616a78871750a659010d6f3769b6b18c9
nicesuits <raion314@outlook.com>
2019-04-03 17:48:12 -0700

    update table component

--- src/components/IssueTable.js
+++ src/components/IssueTable.js
@@ -1,6 +1,29 @@
-import React from 'react';
-import IssueList from './IssueList';
+import React, { Component } from 'react';
+import IssueRow from './IssueRow';
 
-const IssueTable = () => <IssueList />;
+import '../styles/IssueTable.css';
+class IssueTable extends Component {
+  render() {
+    const issueRows = this.props.issues.map(issue => (
+      <IssueRow key={issue.id} issue={issue} />
+    ));
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Completion Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
+    );
+  }
+}
 
 export default IssueTable;
