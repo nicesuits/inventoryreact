c237ae4038a5a5af22c68a069157ed9e005e0958
nicesuits <raion314@outlook.com>
2019-04-03 17:48:22 -0700

    add mock table data

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import IssueFilter from './components/IssueFilter';
 import IssueAdd from './components/IssueAdd';
 import IssueTable from './components/IssueTable';
 
+const data = [
+  {
+    id: 1,
+    status: 'Open',
+    owner: 'Ravan',
+    created: new Date('2016-08-15'),
+    effort: 5,
+    completionDate: undefined,
+    title: 'Error in console when clicking Add'
+  },
+  {
+    id: 2,
+    status: 'Assigned',
+    owner: 'Eddie',
+    created: new Date('2016-08-16'),
+    effort: 14,
+    completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel'
+  }
+];
+
 export default () => (
   <div>
     <h1>Inventory Table</h1>
     <IssueFilter />
     <hr />
-    <IssueTable />
+    <IssueTable issues={data} />
     <hr />
     <IssueAdd />
   </div>
