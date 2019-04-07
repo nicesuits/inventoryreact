e88e1502aaeb01586434f0415e5409aed3aca025
nicesuits <raion314@outlook.com>
2019-04-07 15:40:45 -0700

    add owner query

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -1,6 +1,8 @@
 const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 const issueGraphQLType = require('./issueType');
+const ownerGraphQLType = require('./ownerType');
 const Issue = require('../models/issue');
+const Owner = require('../models/owner');
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -10,6 +12,13 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         return Issue.findById(args.id);
+      }
+    },
+    owner: {
+      type: ownerGraphQLType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return Owner.findById(args.id);
       }
     }
   }
