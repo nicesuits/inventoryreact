878abec2009e0993e9559c24d684a89d912d2a4c
nicesuits <raion314@outlook.com>
2019-04-07 15:40:33 -0700

    update owner with ownerType

--- backend/graphql/issueType.js
+++ backend/graphql/issueType.js
@@ -1,11 +1,22 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLID
+} = require('graphql');
+const OwnerType = require('./ownerType');
 
 const IssueType = new GraphQLObjectType({
   name: 'Issue',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     status: { type: GraphQLString },
-    owner: { type: GraphQLString },
+    owner: {
+      type: OwnerType,
+      resolve(parent, args) {
+        // return_.find(authors, { id: parent.authorId})
+      }
+    },
     created: { type: GraphQLString },
     effort: { type: GraphQLInt },
     completionDate: { type: GraphQLString },
