7367dfa58fe287f5dea502bf83381aafc7674935
nicesuits <raion314@outlook.com>
2019-04-09 20:50:26 -0700

    add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, hashHistory, Switch } from 'react-router-dom';
 import IssueList from './components/IssueList';
+import IssueEdit from './components/IssueEdit';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
-});
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
+const NoMatch = () => <p>No Match Found</p>;
 
 const App = () => (
   <ApolloProvider client={client}>
-    <IssueList />
+    <BrowserRouter history={hashHistory}>
+      <Switch>
+        <Route exact path="/" component={IssueList} />
+        <Route exact path="/issueEdit" component={IssueEdit} />
+        <Route path="*" component={NoMatch} />
+      </Switch>
+    </BrowserRouter>
   </ApolloProvider>
 );
 
